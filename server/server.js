936d9885fb13020d5b1575581146a8d2859ec4af
enimaos <ing.devov@gmail.com>
2024-10-22 01:33:47 +0100

    Project setup : Folders and Initializing

---
+++ server/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mysql = require("mysql");
+const cors = require("cors");
+const path = require("path");
+const app = express();
+//path.resolve()
+app.use(express.static(path.join(__dirname, "public")));
+app.use(cors());
+app.use(express.json());
+
+const port = 5000;
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "pfe",
+});
+
+
+
+app.listen(port, () => {
+    console.log(`listening on port ${port} `);
+});
